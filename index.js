debc04595a1fcaf143b23dba45a2c6ac47da0f96
Johan Oakes <oakejp12@wfu.edu>
2016-07-22 23:01:40 -0400

    Fix function callback to support ES6

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const client = new pushbullet.PushbulletClient();
 
 console.log("Starting the Pushbullet client!");
 
-client.getUser(function() {
-  console.log(data);
-});
+
+client.getUser(() => console.dir(data));

92fdaa57cda0ca7b1961c089cdbe5e0e1147380a
Johan Oakes <oakejp12@wfu.edu>
2016-07-23 15:34:12 -0400

    Call API for sending SMS and build data object

--- index.js
+++ index.js
@@ -6,4 +6,34 @@ console.log("Starting the Pushbullet client!");
 /*
 * Pushbullet client makes a request for user data
 */
-client.getUser(() => console.dir(data));
+// client.getUser(() => console.dir(data));
+
+/*
+* Pushbullet client requests list of chats
+*/
+// client.getDevicesList(() => console.dir(data));
+
+// Object that holds data in order to push SMS from
+// Pushbullet API
+var pushSMS = {
+  "push": {
+    "conversation_iden": "+1 336 596 9718",
+    "message": "Test 1, 2, 3!",
+    "package_name": "com.pushbullet.android",
+    "source_user_iden": "ujyYKqvyziC",
+    "target_device_iden": "ujyYKqvyziCsjAsoeMFET6",
+    "type": "messaging_extension_reply"
+  },
+  "type": "push"
+}
+
+/*
+* TODO: Develop a function that takes command line arguments
+* and adds them to pushSMS
+*/
+
+
+/*
+* Send out a SMS
+*/
+client.sendSMS(pushSMS, console.log("Message sent succesfully!"));

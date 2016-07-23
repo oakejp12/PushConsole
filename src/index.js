c99f0baf8429953e82d12835bceff188204635e8
Johan Oakes <oakejp12@wfu.edu>
2016-07-23 16:32:59 -0400

    Move files to central location

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const config = require('./config');
+const pushbullet = require('./pushbullet');
+const client = new pushbullet.PushbulletClient();
+
+console.log("Starting the Pushbullet client!\n");
+
+
+// Parses the arguments and sets requirements
+var argv = require('yargs')
+  .usage('Trying to send a message?\nUsage: $0 -p <phone number> -m <message>')
+  .alias('p', 'phone')
+  .describe('p', 'Send to phone number')
+  .alias('m', 'message')
+  .describe('m', 'Send this message')
+  .demand(['p', 'm'])
+  .help('h')
+  .alias('h', 'help')
+  .example('$0 -p "335 596 2234" -m "Hey!"')
+  .epilog('Johan Oakes 2016 - Pushbullet')
+  .argv;
+
+/*
+* Pushbullet client makes a request for user data
+*/
+// client.getUser(() => console.dir(data));
+
+/*
+* Pushbullet client requests list of chats
+*/
+// client.getDevicesList(() => console.dir(data));
+
+// Object that holds data in order to push SMS from
+// Pushbullet API
+var pushSMS = {
+  "push": {
+    "conversation_iden": "+1 " + argv.p,
+    "message": argv.m,
+    "package_name": "com.pushbullet.android",
+    "source_user_iden": config.auth.source_user_iden,
+    "target_device_iden": config.auth.target_device_iden,
+    "type": "messaging_extension_reply"
+  },
+  "type": "push"
+}
+
+/*
+* Send out a SMS
+* @param {Object} pushSMS JSON object that holds info to send SMS
+* @param {Function} callback sends success message
+*/
+client.sendSMS(pushSMS, console.log("Message sent succesfully!"));
---
+++ src/pushbullet.js
@@ -0,0 +1,105 @@
+/*
+* @author Johan Oakes
+* Application to send message from phone by way of console
+*/
+const PUSHBULLET_API =  'https://api.pushbullet.com/v2/';
+const request = require('request');
+const config = require('./config');
+
+/*
+* Constructs a new instance of the Pushbullet Client
+* @constructor
+* @this {Pushbullet Client}
+*/
+function PushbulletClient() {
+  const _this = this;
+
+  // The current profile's user id
+  _this.userID = null;
+
+  /*
+  * Helper for getting the request object
+  * @param path {String} path the relative URI path
+  * @param data {Object} data an object of extra values
+  */
+  var getRequestOptions = function(path, data) {
+    var options = {
+      url: PUSHBULLET_API + path,
+      json: data
+    };
+
+    var headers = {
+      'Access-Token' : config.auth.access_token
+    }
+
+    options.headers = headers;
+
+    return options;
+  };
+
+  /*
+  * Issues a GET request to the Pushbullet API
+  * @param {String} path the relative URI path
+  * @param {String} method GET, POST, PUT, etc.
+  * @param {Object} data an object of extra values
+  * @param {Function} callback the callback to invoke when the req completes
+  */
+  var bulletMethod = function(path, method, data, callback) {
+    var opts = getRequestOptions(path, data);
+    opts.method = method;
+    request(opts, callback);
+  };
+
+  /*
+  * Helper for transforming the request callback values
+  * @param {Function} callback the callback to invoke when req completes
+  */
+  var makeBulletCallback = function(callback) {
+    return function(err, res, body) {
+      var data = null;
+
+      if(!err) {
+        if (typeof body === "string") {
+          try {
+            data = JSON.parse(body);
+          } catch (err) {
+            // TODO : Catch the error
+          }
+        } else if (typeof body === "object") data = body;
+      }
+      if (callback) {
+        callback(err, data);
+      }
+    };
+  };
+
+
+  /*
+  * Gets the current users
+  * @param {Object} data
+  * @param {Function} callback
+  */
+  _this.getUser = function(data, callback) {
+    bulletMethod('users/me', 'GET', data, makeBulletCallback((err, data) => console.dir(data)));
+  };
+
+  /*
+  * Get a list of chats belonging to the current user
+  * @param {Object} data
+  * @param {Function} callback callback to invoke when req completes
+  */
+  _this.getDevicesList = function(data, callback) {
+    bulletMethod('devices', 'GET',data, makeBulletCallback((err, data) => console.dir(data)));
+  };
+
+  /*
+  * Send out an SMS
+  */
+  _this.sendSMS = function(data, callback) {
+    bulletMethod('ephemerals', 'POST', data, makeBulletCallback(
+      callback)); // TODO: Revise callback
+  };
+
+};
+
+exports.PushbulletClient = PushbulletClient;

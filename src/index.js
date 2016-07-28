bddc2e91696456b14ce8016aa0787b8818b4580d
Johan Oakes <oakejp12@alumni.wfu.edu>
2016-07-28 09:21:25 -0400

    Add instructions for listing devices

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ var argv = require('yargs')
   .describe('p', 'Send to phone number')
   .alias('m', 'message')
   .describe('m', 'Send this message')
-  .demand(['p', 'm'])
+  // .demand(['p', 'm'])
+  .alias('d', 'devices')
+  .describe('d', 'List device information in JSON')
   .help('h')
   .alias('h', 'help')
   .example('$0 -p "335 596 2234" -m "Hey!"')
@@ -21,35 +23,41 @@ var argv = require('yargs')
   .argv;
 
 /*
+ * Pushbullet client requests list of chats
+ */
+if (argv.d) {
+  console.log('Devices:\n');
+  client.getDevicesList((err, data) => console.dir(data['devices']));
+}
+
+
+/*
 * Pushbullet client makes a request for user data and
 * passes the data to a callback function that initiates SMS
 */
-client.getUserIdentity(function (err, data) {
-  // Object that holds data so that Pushbullet
-  // knows how to configure the SMS
-  if (!err) {
-    var pushSMS = {
-      'push': {
-        'conversation_iden': `+1  ${argv.p}`,
-        'message': argv.m,
-        'package_name': 'com.pushbullet.android',
-        'source_user_iden': data['iden'],
-        'target_device_iden': config.auth.target_device_iden,
-        'type': 'messaging_extension_reply'
-      },
-      'type': 'push'
-    };
+if (argv.p && argv.m) {
+  client.getUserIdentity(function (err, data) {
+    // Object that holds data so that Pushbullet
+    // knows how to configure the SMS
+    if (!err) {
+      var pushSMS = {
+        'push': {
+          'conversation_iden': `+1  ${argv.p}`,
+          'message': argv.m,
+          'package_name': 'com.pushbullet.android',
+          'source_user_iden': data['iden'],
+          'target_device_iden': config.auth.target_device_iden,
+          'type': 'messaging_extension_reply'
+        },
+        'type': 'push'
+      };
 
-    /*
-     * Send out a SMS
-     * @param {Object} pushSMS JSON object that holds info to send SMS
-     * @param {Function} callback sends success message
-     */
-    client.sendSMS(pushSMS, console.log('Message sent successfully!'));
-  } else console.error(err);
-});
-
-/*
-* Pushbullet client requests list of chats
-*/
-// client.getDevicesList((err, data) => console.dir(data));
+      /*
+       * Send out a SMS
+       * @param {Object} pushSMS JSON object that holds info to send SMS
+       * @param {Function} callback sends success message
+       */
+      client.sendSMS(pushSMS, console.log('Message sent successfully!\n'));
+    } else console.error(err);
+  });
+}
